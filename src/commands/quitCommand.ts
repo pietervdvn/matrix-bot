e3f9779db31aeafa579d5ce04c76fcaa63facca9
pietervdvn <pietervdvn@posteo.net>
2022-05-21 01:57:34 +0200

    Add update to exit command

--- src/commands/quitCommand.ts
+++ src/commands/quitCommand.ts
@@ -1,5 +1,6 @@
 import {Command} from "../command";
 import {ResponseSender} from "../ResponseSender";
+import {exec} from "child_process"
 
 export class QuitCommand extends Command<{ mode: "restart" | "shutdown" | string }> {
 
@@ -18,18 +19,44 @@ export class QuitCommand extends Command<{ mode: "restart" | "shutdown" | string
 
 
     public async Run(r: ResponseSender, args: {
-        mode: "restart" | "shutdown" | string,
+        mode: "update" | "shutdown" | string,
         _: string
     }): Promise<"shutdown"> {
         if ((new Date().getTime() - QuitCommand.inited.getTime()) < 1000) {
             await r.sendNotice("Not yet shutting down - to soon (" + (QuitCommand.inited.getTime() - new Date().getTime()) + " ms since launch, needs 1000)")
             return;
         }
+        if(args.mode === "update"){
+            console.log("Received 'update' command, updating...")
+            await r.sendNotice("Updating MapComplete...")
+
+            const process = exec("cd MapComplete && git pull", ((error, stdout, stderr) => {
+                r.sendHtml("StdOut gave: <code>"+stdout+"</code>")
+                
+                if (error !== null) {
+                    console.error(error)
+                    r.sendHtml("Updating gave an error: <code>"+error+"</code>")
+                }
+                if (stderr !== "") {
+                    console.error(stderr)
+                    r.sendHtml("Updating gave an error (via StdErr): <code>"+stderr+"</code>")
+                }
+                
+            }))
+
+
+            process.on("exit", () => r.sendHtml("All done"))
+            
+            args.mode = "shutdown"
+
+        }
+        
         if (args.mode === "shutdown") {
             console.log("Received 'shutdown' command, exiting now")
             await r.sendNotice("Shutting down... See you later!")
             return "shutdown"
         }
+
         await r.sendNotice("Invalid quit command")
     }
 
